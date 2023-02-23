6249df19bb8150650a75125e84cdccee1b13c6cf
iamfrans <1323544@senati.pe>
2023-02-23 18:52:47 -0500

    elaboracion de algoritmo

--- algoritomo de suma y producto.py
+++ algoritomo de suma y producto.py
@@ -1,3 +1,7 @@
 #Agregamos la funcion input para introducir datos desde el teclado 
 primer_valor = int(input("Ingrese el pimr valor: "))
 segundo_valor = int(input("ingrese el segundo valor: "))
+
+#Establecemos  un algoritmo que al ingresar valores nos de la suma y el producto :v
+suma = primer_valor + segundo_valor
+producto = primer_valor * segundo_valor 
