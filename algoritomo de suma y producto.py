afd0dd30fe8160b7b6cddb67f643115ffeaa8bf3
iamfrans <1323544@senati.pe>
2023-02-23 18:57:00 -0500

    Incorporacion de la funcion print y str para la finalizacion del algoritmo

--- algoritomo de suma y producto.py
+++ algoritomo de suma y producto.py
@@ -5,3 +5,7 @@ segundo_valor = int(input("ingrese el segundo valor: "))
 #Establecemos  un algoritmo que al ingresar valores nos de la suma y el producto :v
 suma = primer_valor + segundo_valor
 producto = primer_valor * segundo_valor 
+
+#Añadimos la funcion print para mostrar la informacion deseada con la funcion str <3 
+print("la suma es:", str(suma))
+print("El producto es:" + str(producto))
