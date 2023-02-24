2c1b47e52feea0926f079f6a528c5fc074fd86e6
Iamfrans <1323544@senati.pe>
2023-02-24 10:19:51 -0500

    funcion for

--- suma usando funciones de bucle/funcion_bucle.py
+++ suma usando funciones de bucle/funcion_bucle.py
@@ -1,2 +1,6 @@
 #usamos la funcion input para ingresar datos en int
 n=int(input("ingrese rango:"))
+#usamos la fucion for para el bucle
+#no entendi bien la exlicacion de mi compa
+for i in range (1,n+1):
+     print(i,"+",i-1, "=",i+(i-1))
