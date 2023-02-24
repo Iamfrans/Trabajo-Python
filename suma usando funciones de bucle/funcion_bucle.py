99c04d771f9573f477dbdc544151391924e8dbb1
Iamfrans <1323544@senati.pe>
2023-02-24 10:24:43 -0500

    las comillas

--- suma usando funciones de bucle/funcion_bucle.py
+++ suma usando funciones de bucle/funcion_bucle.py
@@ -1,7 +1,7 @@
 #usamos la funcion input para ingresar datos en int
 n=int(input("ingrese rango:"))
 #implemnamos un print para la stetica
-print(0 + 0 = 0)
+print("0 + 0 = 0")
 #usamos la fucion for para el bucle
 #no entendi bien la exlicacion de mi compa
 for i in range (1,n+1):
