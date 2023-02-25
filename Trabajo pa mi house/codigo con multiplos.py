e5225ba1b5ddb1b1bc41baed85c34c329a8844d9
iamfrans <1323544@senati.pe>
2023-02-24 23:10:42 -0500

    agregamos lista

--- Trabajo pa mi house/codigo con multiplos.py
+++ Trabajo pa mi house/codigo con multiplos.py
@@ -1,8 +1,12 @@
 #usamos la funcion def
 def x(lista): 
     resultado = []
-#funcion for
-  for n in lista: 
+#   funcion for 
+    for n in lista: 
         if n % 5 == 0: 
             resultado.append(n)
     return resultado 
+#agregamos la lista 
+numeros = [5,150,40,27,89,65,55,80,30]
+y = x(numeros)
+print(y)
